d201e5ede5e99fbc86c30adf256c36c3a6101260
Guglielmo Turco <>
2021-05-24 13:58:45 -0700

    Connected data to table body component

--- src/components/TableBody/index.js
+++ src/components/TableBody/index.js
@@ -1,18 +1,53 @@
-import React from 'react';
+
 import Tbody from '../Tbody';
 import Tr from '../Tr';
 import Td from '../Td';
+import classnames from 'classnames';
+import { arrayOf, string, object } from 'prop-types';
 
-
-const TableBody = props => {
+const TableBody = ({
+    id,
+    data,
+    columns,
+    className,
+    ...remainingProps
+}) => {
 
     return (
-        <Tbody>
-            <Tr>
-                <Td>data here!</Td>
-            </Tr>
+        <Tbody
+            {...remainingProps}
+            className={classnames(className)}
+        >
+            {
+                data.map((currRow, i) =>
+                    <Tr
+                        key={`row-${id}-${i}`}
+                    >
+                        {columns.map((currCol, i) => {
+
+                            let key = `col-${id}-${i}`;
+                            return (
+                                <Td
+                                    key={key}
+                                    id={key}
+                                >
+                                    {currRow[currCol.accessor]}
+                                </Td>
+                            )
+                        })}
+                    </Tr>
+                )
+            }
+
         </Tbody>
     )
 }
 
 export default TableBody;
+
+TableBody.propTypes = {
+    id: string,
+    data: arrayOf(object),
+    columns: arrayOf(object),
+    className: string,
+}
