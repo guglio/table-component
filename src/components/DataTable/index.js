f509a4e3fd40110a0fcae69a01722fd795bb8355
Guglielmo Turco <>
2021-05-24 13:55:30 -0700

    Connecting the header data with component

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -2,22 +2,26 @@ import React from 'react';
 import Table from '../Table';
 import TableHeader from '../TableHeader';
 import TableBody from '../TableBody';
-import { arrayOf, object } from 'prop-types';
+import { arrayOf, object , string} from 'prop-types';
 
 const DataTable = ({
-    data,
-    columns,
+    data = [],
+    columns = [],
+    id,
     ...remainingProps
 }) => {
+
     return (<>
         <div>Action Bar Here</div>
         <Table
             {...remainingProps}
         >
-            <TableHeader 
+            <TableHeader
+                id={id} 
                 columns={columns}
             />
             <TableBody
+                id={id} 
                 data={data}
                 columns={columns}
             />
@@ -31,5 +35,6 @@ export default DataTable;
 
 DataTable.propTypes = {
     data: arrayOf(object),
-    columns: arrayOf(object)
+    columns: arrayOf(object),
+    id:string
 }
--- src/components/TableHeader/index.js
+++ src/components/TableHeader/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React from 'react'
 import Thead from '../Thead';
 import Tr from '../Tr';
 import Th from '../Th';
+import classnames from 'classnames';
+import { arrayOf, string, object } from 'prop-types';
 
-const TableHeader = props => {
-    return <Thead>
-                <Tr>
-                    <Th>Header</Th>
-                </Tr>
-            </Thead>
+const TableHeader = ({
+    columns = [],
+    id,
+    className,
+    ...remainingProps
+}) => {
+    return (
+        <Thead {...remainingProps} className={classnames(className)}>
+            <Tr className="header-row">
+                {columns.map((item, i) => {
+                    let key = `header-col-${id}-${i}`;
+                    return (
+                        <Th
+                            key={key}
+                            id={key}
+                            className={classnames(item.className)}
+                        >
+                            {item.Header}
+                        </Th>
+                    )
+                })}
+            </Tr>
+        </Thead>)
 }
 
 export default TableHeader;
+
+TableHeader.propTypes = {
+    columns: arrayOf(object),
+    id: string,
+    className: string
+}
