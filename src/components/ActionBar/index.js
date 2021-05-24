89b66ac43acc2e06ab0b227e11e4d7218bf71184
Guglielmo Turco <>
2021-05-24 15:46:21 -0700

    Add initial styles and icon for download button

--- src/components/ActionBar/index.js
+++ src/components/ActionBar/index.js
@@ -1,3 +1,5 @@
+import React, { useCallback } from 'react';
+import { ReactComponent as DownloadIcon } from '../../assetts/file_download_black_24dp.svg';
 import { number, arrayOf, object, bool, string, func } from 'prop-types';
 import './action-bar.css';
 
@@ -8,9 +10,25 @@ const ActionBar = ({
     data,
     multiSelect
 }) => {
-    return <div>
-        <input type="checkbox"></input>
-        <button>Download</button>
+
+    const selectedRowsLength = useCallback(() => selectedRows.filter(r => r).length, [selectedRows]);
+
+    return <div className='action-bar'>
+        <div>
+            <input type="checkbox"></input>
+        </div>
+        <div className="selected-count">
+            {
+                selectedRowsLength() > 0
+                    ?
+                    `Selected ${selectedRowsLength()}`
+                    :
+                    'None Selected'
+            }
+        </div>
+        <div>
+            <button><DownloadIcon /> Download</button>
+        </div>
     </div>
 }
 
